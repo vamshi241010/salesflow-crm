66e5c527dc6d6438aa9223cddbe1112b32135333
vamshi241010 <vamshi242010@gmail.com>
2026-05-24 00:30:37 +0530

    Fixed production API routes

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://salesflow-backend-ld7k.onrender.com",
+  baseURL:
+    "https://salesflow-backend-ld7k.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
+
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization =
+      `Bearer ${token}`;
   }
 
   return config;
